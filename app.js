c8fa0295006d0ca870e4a823011d99924988848f
Tom Kim <tom.kim@dramancompany.com>
2017-10-26 21:12:56 +0900

    beta code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('A project입니다!');
+  res.send('BETA 버전의 A project입니다!');
 });
 
 app.listen(3000, function () {
